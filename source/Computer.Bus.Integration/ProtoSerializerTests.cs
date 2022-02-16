bb8db74cb01515ac4bc15b00352fe3d91b5d4f18
David Squire <squiredd@gmail.com>
2022-02-15 21:58:39 -0800

    add serialization test

---
+++ source/Computer.Bus.Integration/ProtoSerializerTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Computer.Bus.ProtobuffNet;
+using NUnit.Framework;
+using ProtoBuf;
+
+namespace Computer.Bus.Integration;
+
+public class ProtoSerializerTests
+{
+    private ProtoSerializer _protoSerializer;
+    [SetUp]
+    public void Setup()
+    {
+        _protoSerializer = new ProtoSerializer();
+    }
+
+    /// <summary>
+    /// The lesson here is to use Concrete types of lists on the receiving end
+    /// </summary>
+    /// <exception cref="NullReferenceException"></exception>
+    [Test]
+    public async Task Test1()
+    {
+        var response = new DefaultListResponse
+        {
+            List = new ListModel
+            {
+                Id = "some app id",
+                Items = new List<ItemModel>
+                {
+                    new ItemModel
+                    {
+                        Checked = true,
+                        Text = "Success!!!"
+                    }
+                }
+            },
+            Success = true
+        };
+
+        string eventId = Guid.NewGuid().ToString();
+        string correlationId = Guid.NewGuid().ToString();
+        var bytes = await _protoSerializer.Serialize(response, typeof(DefaultListResponse), eventId, correlationId).ConfigureAwait(false);
+
+        if (bytes == null)
+        {
+            throw new NullReferenceException("something");
+        }
+        var result = await _protoSerializer.Deserialize(bytes, typeof(DefaultListResponse)).ConfigureAwait(false);
+
+        var list = (DefaultListResponse?)result?.Payload;
+        Assert.AreEqual("Success!!!", list?.List?.Items.FirstOrDefault()?.Text);
+    }
+    
+    [ProtoContract]
+    public class ItemModel
+    {
+        [ProtoMember(1)]
+        public string? Text { get; set; } = null;
+        [ProtoMember(2)]
+        public string? Url { get; set; } = null;
+        [ProtoMember(3)]
+        public string? ImageUrl { get; set; } = null;
+        [ProtoMember(4)]
+        public bool? Checked { get; set; } = false;
+    }
+    [ProtoContract]
+    public class ListModel
+    {
+        [ProtoMember(1)]
+        public string? Id { get; set; } = "not set";
+        [ProtoMember(2)]
+        public List<ItemModel>? Items { get; set; } = new List<ItemModel>();
+    }
+    [ProtoContract]
+    public class DefaultListResponse
+    {
+        [ProtoMember(1)]
+        public bool? Success { get; init; }
+
+        [ProtoMember(2)]
+        public ListModel? List { get; init; }
+    }
+}
